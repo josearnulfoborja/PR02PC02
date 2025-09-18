916e504822d6736be247daed36af0df3888f9322
Jose Borja <josearnulfoborja@hotmail.com>
2025-09-17 19:25:49 -0600

    Creacion de proyecto
    Creacion de proyecto

---
+++ PR02PC02/src/main/java/com/mycompany/pr02pc02/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.pr02pc02;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
