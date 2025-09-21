8f4641090c8ec0f65ea922b3212046483fdddfc4
k4r3nRod <kar3n.rodri18@gmail.com>
2025-09-21 13:42:22 -0600

    sa

---
+++ PR02PC02/src/main/java/com/mycompany/pr02pc02/servlets/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.pr02pc02.servlets;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
